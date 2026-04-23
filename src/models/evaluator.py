003eacacabe1367302eaa61daea67b6b867202ea
Daniel Mai <getwhattt@gmail.com>
2026-04-23 11:30:09 +0700

    New evaluate functions

--- src/models/evaluator.py
+++ src/models/evaluator.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import (
-    classification_report, roc_auc_score, confusion_matrix, 
-    average_precision_score, precision_recall_curve
+    roc_auc_score, confusion_matrix, average_precision_score, 
+    precision_recall_curve, fbeta_score,
+    precision_score, recall_score
     )
 import matplotlib.pyplot as plt
 
@@ -8,14 +9,14 @@ def evaluate_model(model, X_test, y_test, name="Model"):
     y_pred = model.predict(X_test)
     y_prob = model.predict_proba(X_test)[:, 1]
 
-    print(f"\n🔹 {name}")
-    print("-" * 40)
+    print(f"\n-- {name} --")
     
-    print("Classification Report:")
-    print(classification_report(y_test, y_pred))
+    print(f"  Precision : {precision_score(y_test, y_pred):.4f}")
+    print(f"  Recall    : {recall_score(y_test, y_pred):.4f}")
+    print(f"  F2-score  : {fbeta_score(y_test, y_pred, beta=2):.4f}")
     
-    print("ROC-AUC:", roc_auc_score(y_test, y_prob))
-    print("PR-AUC:", average_precision_score(y_test, y_prob))
+    print(f"  ROC-AUC   : {roc_auc_score(y_test, y_prob):.5f}")
+    print(f"  PR-AUC    : {average_precision_score(y_test, y_prob):.5f}")
     
     print("Confusion Matrix:")
     print(confusion_matrix(y_test, y_pred))
@@ -24,13 +25,14 @@ def evaluate_threshold(model, X_test, y_test, threshold=0.3, name="Model"):
     y_prob = model.predict_proba(X_test)[:, 1]
     y_pred = (y_prob > threshold).astype(int)
 
-    print(f"\n🔹 {name} (Threshold = {threshold})")
-    print("-" * 40)
-    print(classification_report(y_test, y_pred))
+    print(f"\n-- {name} (Threshold = {threshold}) --")
+    print(f"  Precision : {precision_score(y_test, y_pred):.4f}")
+    print(f"  Recall    : {recall_score(y_test, y_pred):.4f}")
+    print(f"  F2-score  : {fbeta_score(y_test, y_pred, beta=2):.4f}")
 
-    print("ROC-AUC:", roc_auc_score(y_test, y_prob))
-    print("PR-AUC:", average_precision_score(y_test, y_prob))
-    
+    print(f"  ROC-AUC   : {roc_auc_score(y_test, y_prob):.5f}")
+    print(f"  PR-AUC    : {average_precision_score(y_test, y_prob):.5f}")   
+
     print("Confusion Matrix:")
     print(confusion_matrix(y_test, y_pred))
 
